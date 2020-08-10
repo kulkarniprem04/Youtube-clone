cedfffb7f5291ab44ca0c038a870ab971e6d1728
Prem kulkarni <kulkarniprem04@gmail.com>
2020-08-10 22:41:17 +0530

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,59 +1,72 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
-import './App.css';
-import Header from './Header.js';
-import Sidebar from './Sidebar.js';
-import Recommended from './Recomended.js';
-import Searchpage from './Searchpage';
-import youtube from './youtube.js';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import Header from "./Header.js";
+import Sidebar from "./Sidebar.js";
+import Recommended from "./Recomended.js";
+import Searchpage from "./Searchpage";
+//import youtube from './youtube.js';
 
 function App() {
-  const [clicked, setClicked] = useState(false)
-  const [darkmode, setDarkmode] = useState(false) 
+  const API_KEY = 'AIzaSyAlnrOCW5kTF67bddvxA8boYmrFi3-3jNA';
+  const [clicked, setClicked] = useState(false);
+  const [darkmode, setDarkmode] = useState(false);
 
   const menuIconSelect = () => {
-    setClicked(!clicked)
-  }
+    setClicked(!clicked);
+  };
 
   const toggleChecked = () => {
     setDarkmode((prev) => !prev);
   };
 
-  const handleFormSubmit = async (searchterm) => {
-    const response = await youtube.get('/search', {
-        params: {
-            q: searchterm
-        }
-    })
+  const params = new URLSearchParams({
+    part: 'snippet',
+    maxResults: 10,
+    key: API_KEY ,
+    type: 'video' 
+  })
 
-    console.log(response)
-}
+
+  const handleFormSubmit = async (searchterm) => {
+    await fetch(`https://www.googleapis.com/youtube/v3/search?q=${searchterm}${ params.toString()}`)
+      .then((response) => response.json())
+      .then(console.log)
+  };
 
   return (
-          <>
-            <Router>
-              <Header handleFormSubmit={handleFormSubmit} darkmode={darkmode} clicked={clicked} menuIconSelect={menuIconSelect}/>
-              <Switch>
-                <Route path="/search/:searchTerm">
-                <div className="App_page">
-                  <Sidebar darkmode={darkmode} toggleChecked={toggleChecked} clicked={clicked} />
-                  <Searchpage />    
-                </div>
-                </Route>
-                <Route path="/">
-                <div className="App_page">
-                  <Sidebar darkmode={darkmode} toggleChecked={toggleChecked} clicked={clicked} />
-                  <Recommended darkmode={darkmode} clicked={clicked}/>    
-                </div>
-                </Route>
-              </Switch>
-            </Router>
-
-
-
-            
-          </>
-        
+    <>
+      <Router>
+        <Header
+          handleFormSubmit={handleFormSubmit}
+          darkmode={darkmode}
+          clicked={clicked}
+          menuIconSelect={menuIconSelect}
+        />
+        <Switch>
+          <Route path="/search/:searchTerm">
+            <div className="App_page">
+              <Sidebar
+                darkmode={darkmode}
+                toggleChecked={toggleChecked}
+                clicked={clicked}
+              />
+              <Searchpage />
+            </div>
+          </Route>
+          <Route path="/">
+            <div className="App_page">
+              <Sidebar
+                darkmode={darkmode}
+                toggleChecked={toggleChecked}
+                clicked={clicked}
+              />
+              <Recommended darkmode={darkmode} clicked={clicked} />
+            </div>
+          </Route>
+        </Switch>
+      </Router>
+    </>
   );
 }
 
--- src/youtube.js
+++ src/youtube.js
@@ -1,12 +1,11 @@
-import axios from 'axios';
-const KEY = 'AIzaSyBO_F93b9bqvrnpXYsVsJvJ1dr-ouT8PHY';
-
+import axios from "axios";
+//const KEY = 'AIzaSyBO_F93b9bqvrnpXYsVsJvJ1dr-ouT8PHY';
 
 export default axios.create({
-    baseURL: 'https://www.googleapis.com/youtube/v3/',
-    params: {
-        part: 'snippet',
-        maxResults: 10,
-        key: KEY
-    }
-})
+  baseURL: "https://www.googleapis.com/youtube/v3/",
+  params: {
+    part: "snippet",
+    maxResults: 10,
+    key: "AIzaSyBO_F93b9bqvrnpXYsVsJvJ1dr-ouT8PHY"
+  }
+});
